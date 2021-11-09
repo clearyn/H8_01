3bcfff55931d872e8cfb2386ed4408a338262e7c
Ryan Cleary <49626962+clearyn@users.noreply.github.com>
2021-11-09 19:26:07 +0700

    Finish assignment

--- SESI03/Assignment/003_h8ocbc_converter.py
+++ SESI03/Assignment/003_h8ocbc_converter.py
@@ -1,6 +1,12 @@
+"""
+003_h8ocboc_converter
+Nama: Ryan Cleary
+Kode: FSDO001ONL003
+"""
+
 def kelvin_to_celcius(value: float, reverse: bool = False):
     """
-    kelvin_to_celcius(value, reverse) is function to convert kelvin to celcius temperature, this function can be reversed by add value "True on reverse argument"
+    kelvin_to_celcius(value, reverse) is function to convert Kelvin to Celcius temperature, this function can be reversed by add value "True on reverse argument"
     :param value: float | integer
     :param reverse: bool | default: False 
     :return return_value: float of converted value temperature
@@ -14,17 +20,13 @@ def kelvin_to_celcius(value: float, reverse: bool = False):
     return_value = round(value, 2)
     return return_value
 
-print(kelvin_to_celcius(50))
-print(kelvin_to_celcius(50, True))
-
-
 def toFahrenheit(value: float, unit: str):
     """
-    toFahrenheit(value, unit) is function to convert kelvin or celcius unit temperature into fahrenheit unit
+    toFahrenheit(value, unit) is function to convert Celcius or Kelvin unit temperature into Fahrenheit unit
     :param value: float | integer
     :param unit: str
     :assert InvalidUnit: if unit value not 'celcius' or 'kelvin'
-    :return return_value: float of converted value temperature
+    :return return_value: float of converted value Celcius or Kelvin into Fahrenheit 
     """
     assert unit in ['celcius', 'kelvin'], "InvalidUnit: unit value only accept 'celcius' or 'kelvin'"
     # Celcius to Fahrenheit formula: (0°C × 9/5) + 32 = 32°F (Google Unit Converter)
@@ -32,16 +34,13 @@ def toFahrenheit(value: float, unit: str):
     return_value = round(((celcius * 9/5) + 32), 2)
     return return_value
 
-print(toFahrenheit(50, 'celcius'))
-print(toFahrenheit(50, 'kelvin'))
-
 def fahrenheit_to_cf(value: float, output: str):
     """
-    fahrenheit_to_cf(value, unit) is function to convert kelvin or celcius unit temperature into fahrenheit unit
+    fahrenheit_to_cf(value, unit) is function to convert Fahrenheit unit temperature into Celcius or Kelvin unit
     :param value: float | integer
     :param output: str
-    :assert InvalidUnit: if unit value not 'celcius' or 'kelvin'
-    :return return_value: float of converted value temperature
+    :assert InvalidOutput: if output value not 'celcius' or 'kelvin'
+    :return return_value: float of converted value Fahrenheit into Celcius or Kelvin
     """
     assert output in ['celcius', 'kelvin'], "InvalidOutput: unit value only accept output 'celcius' or 'kelvin'"
     # Fahrenheit to Celcius formula: (0°F − 32) × 5/9 = -17.78°C (Google Unit Converter)
@@ -50,5 +49,59 @@ def fahrenheit_to_cf(value: float, output: str):
     return_value = round(temp_value, 2)
     return return_value
 
-print(fahrenheit_to_cf(50, 'celcius'))
-print(fahrenheit_to_cf(50, 'kelvin'))
+##
+print(__doc__)
+isFinished = False
+defaultValueTest = 50
+intro = """
+Choose menu:
+1. I want to convert kelvin to celcius
+2. I want to convert celcius to kelvin
+3. I want to convert celcius to fahrenheit
+4. I want to convert kelvin to fahrenheit
+5. I want to convert fahrenheit to celcius
+6. I want to convert fahrenheit to kelvin
+7. Auto test all with my value
+8. Exit
+"""
+
+while isFinished == False:
+    try:
+        print(intro)
+        choice = int(input("Enter number of choice: "))
+        if(choice == 1): 
+            i_value = float(input("Enter Kelvin value: "))
+            print("Your value in Celcius : {result}".format(result = kelvin_to_celcius(i_value)))
+        if(choice == 2): 
+            i_value = float(input("Enter Celcius value: "))
+            print("Your value in Kelvin : {result}".format(result = kelvin_to_celcius(i_value, reverse = True)))
+        if(choice == 3): 
+            i_value = float(input("Enter Celcius value: "))
+            print("Your value in Fahrenheit : {result}".format(result = toFahrenheit(i_value, 'celcius')))
+        if(choice == 4): 
+            i_value = float(input("Enter Kelvin value: "))
+            print("Your value in Fahrenheit : {result}".format(result = toFahrenheit(i_value, 'kelvin')))
+        if(choice == 5): 
+            i_value = float(input("Enter Fahrenheit value: "))
+            print("Your value in Celcius : {result}".format(result = fahrenheit_to_cf(i_value, 'celcius')))
+        if(choice == 6): 
+            i_value = float(input("Enter Fahrenheit value: "))
+            print("Your value in Kelvin : {result}".format(result = fahrenheit_to_cf(i_value, 'kelvin')))
+        if(choice == 7):
+            defaultValueTest = float(input("Enter value for all test: "))
+            print("\nShowing all test result :\n")
+            print(defaultValueTest,"K -> C = ",  kelvin_to_celcius(defaultValueTest))
+            print(defaultValueTest,"C -> K = ",  kelvin_to_celcius(defaultValueTest, True))
+            print(defaultValueTest,"C -> F = ",  toFahrenheit(defaultValueTest, 'celcius'))
+            print(defaultValueTest,"K -> F = ",  toFahrenheit(defaultValueTest, 'kelvin'))
+            print(defaultValueTest,"F -> C = ",  fahrenheit_to_cf(defaultValueTest, 'celcius'))
+            print(defaultValueTest,"F -> K = ",  fahrenheit_to_cf(defaultValueTest, 'kelvin'))
+        if choice == 8: isFinished = True
+
+        input("\nPress enter to continue")
+
+    except:
+        print("Something went wrong, try again with valid number value")
+        continue
+else:
+    print("Terima Kasih, testing selesai")
---
+++ SESI03/Assignment/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+fahrenheit_to_cf(i_value, 'celcius')))
+        if(choice == 6): 
