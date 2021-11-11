52957de4554946a66c46bfb31081b1db60edd0db
Ryan Cleary <49626962+clearyn@users.noreply.github.com>
2021-11-11 15:57:56 +0700

    add sesi 07 tambahan

--- SESI07/myproject/HelloWorld.py
+++ SESI07/myproject/HelloWorld.py
@@ -1,16 +1,21 @@
-from types import MethodType
 from flask import Flask, request, render_template, url_for
 from markupsafe import escape
+import webbrowser
+from author_book import author_book
 
+##Create the application instance (Include template html)
 app = Flask(__name__, template_folder='templates')
 
+##Custom generator link for homepage
 def indexLink():
     yield url_for('index')
     yield url_for('show_user_profile', username = 'Wawan Reginold')
     yield url_for('show_post', post_id = 1)
     yield url_for('show_subpath', subpath = 1)
     yield url_for('login', method='POST')
+    yield url_for('author')
 
+##Routing
 @app.route('/')
 def index():
     links = []
@@ -28,7 +33,15 @@ def show_post(post_id):    # show the post with the given id, the id is an integ
 @app.route('/path/<path:subpath>')
 def show_subpath(subpath):    # show the subpath after /path/
     return f'Subpath {escape(subpath)}'
-@app.route('/path/anotherpath')
+
+##Another example book
+@app.route('/author', methods=['GET', 'POST'])
+def author():
+    if 'author_id' in request.form:
+        author_book[request.form['author_id']] = []
+
+    return render_template('authors.html', author_book = author_book)
+
 
 ##HTTP Methods
 @app.route('/login', methods=['GET', 'POST'])
@@ -38,5 +51,6 @@ def login():
     else:       
         return 'show_the_login_form()'
     
-if __name__ == '__main__':    
+if __name__ == '__main__':
+    webbrowser.open_new('http://127.0.0.1:5000/')
     app.run(debug=True)
---
+++ SESI07/myproject/author_book.py
@@ -0,0 +1,1 @@
+author_book = {'author_id' : 100, 'author_id' : 200, 'author_id' : 300}
